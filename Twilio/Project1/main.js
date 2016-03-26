9f5d6611c78781fd362f4b8e227e0d63c2dc5cf5
slydog123123 <elisha@coad.net>
2016-03-26 08:05:22 +0000

    test

--- Twilio/Project1/main.js
+++
@@ -1,6 +0,0 @@
-Twilio.callAndSay("208-691-3546", "Courtesy of EEEEEE beeep, cow, toaster, sock, mind, airplane, destiny, index, water, spAAAAAAAAAAAAAAAce");
-setTimeout(function() {
-  Twilio.callAndPlay("208-691-3546", "https://preview.c9users.io/slydog123123/projects/Twilio/Project2/twenty%20one%20pilots%20-%20Migraine.mp3?_c9_id=livepreview2&_c9_host=https://ide.c9.io");
-}, 30000);
-
-//Twilio.callAndSay("208-691-3546", "hello")
