0785dfc3fe767adabe223961363f5a5fbbb8e1c9
Elisha Coad <elisha@coad.net>
2017-04-29 01:56:32 -0700

    Update

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     window.sr = ScrollReveal();
-    sr.reveal('.tile', { duration: 1000 } , 500);
+    sr.reveal('.tile', { duration: 1000 }, 500);
 
     $(".slight-tilt").rotate({
         bind: {
@@ -14,23 +14,30 @@ $(document).ready(function() {
     });
 
 
+// $('main').fadeIn(1000);
 
-    $('main').fadeIn(1000);
+// window.onload = function() {
+//     $('main').show();
+// }
 
-    $('.navbar a, .tile a').click(function(event) {
-    event.preventDefault();
-    newLocation = this.href;
-    $('main').fadeOut(300, newpage);
-    });
+// $('.navbar a, .tile a').click(function(event) {
+//     event.preventDefault();
+//     newLocation = this.href;
+//     $('main').fadeOut(300, newpage);
+// });
 
-    function newpage() {
+function newpage() {
     window.location = newLocation;
-    }
-    // $(window).fancy_scroll({
-    //     animation: "bounce", // Options available are bounce (like on iOS), or glow (like on Android 4.0+)
-    //     bounceDistance: 50, // ONLY for bounce effect: The distance in pixels that page will overflow. Default is 50 pixels.
-    //     glowColor: "#02A1FA", // ONLY for glow effect: The colour of the glow effect. Default is #02A1FA (blueish).
-    //     animDuration: "0.3s", // The animation duration. Can take milliseconds (200ms) or seconds value (0.2s). Default is 0.2s
-    //     animEasing: "cubic-bezier(0.175, 0.885, 0.420, 1.310)", // This field accept css easing options. Options available are: linear, ease-in, ease-out, ease-in-out or you can generate it with the tool available here: http://matthewlein.com/ceaser/
-    // });
+}
+
+// $(window).fancy_scroll({
+//     animation: "bounce", // Options available are bounce (like on iOS), or glow (like on Android 4.0+)
+//     bounceDistance: 50, // ONLY for bounce effect: The distance in pixels that page will overflow. Default is 50 pixels.
+//     glowColor: "#02A1FA", // ONLY for glow effect: The colour of the glow effect. Default is #02A1FA (blueish).
+//     animDuration: "0.3s", // The animation duration. Can take milliseconds (200ms) or seconds value (0.2s). Default is 0.2s
+//     animEasing: "cubic-bezier(0.175, 0.885, 0.420, 1.310)", // This field accept css easing options. Options available are: linear, ease-in, ease-out, ease-in-out or you can generate it with the tool available here: http://matthewlein.com/ceaser/
+// });
+
+
 });
+
